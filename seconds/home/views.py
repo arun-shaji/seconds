142ba9b858d3ae8292fa3ae7995deac5552f1136
arun <arunshaji4230@gmail.com>
2017-01-30 21:43:23 +0530

    Implemented home page with absic functionalities

--- seconds/home/views.py
+++ seconds/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class IndexView(View):
+    def __init__(self):
+        super(IndexView, self).__init__()
+
+    @classmethod
+    def get(cls, request):
+        return render(request, 'home/index.html', context={})
